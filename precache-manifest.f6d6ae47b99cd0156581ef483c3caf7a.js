f5670f013c0ca365ab1476fc62fcaee969490b98
Luong Minh Tam <tam.luong-minh@banvien.com.vn>
2020-04-03 14:55:31 +0700

    Updates

---
+++ precache-manifest.f6d6ae47b99cd0156581ef483c3caf7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f06cadc980dfd836383976fd0938306",
+    "url": "/dau-giau/index.html"
+  },
+  {
+    "revision": "cc5f104dc0932493c34d",
+    "url": "/dau-giau/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f8b3fb48d994548593c1",
+    "url": "/dau-giau/static/js/2.d62649a5.chunk.js"
+  },
+  {
+    "revision": "c03a38235b49ce3de8834e76429c16a7",
+    "url": "/dau-giau/static/js/2.d62649a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc5f104dc0932493c34d",
+    "url": "/dau-giau/static/js/main.2798e138.chunk.js"
+  },
+  {
+    "revision": "33fff83a7190b22cbdff",
+    "url": "/dau-giau/static/js/runtime-main.1ee8062f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dau-giau/static/media/logo.5d5d9eef.svg"
+  }
+]);
