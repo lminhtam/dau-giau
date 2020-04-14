96fc4697c8dce6cde949e27758f7487ccf855883
Luong Minh Tam <tam.luong-minh@banvien.com.vn>
2020-04-15 03:12:45 +0700

    fix project page component

--- src/pages/Project/component/articleButton.jsx
+++ src/pages/Project/component/articleButton.jsx
@@ -1,25 +1,46 @@
 import React from "react";
-import "../project.css";
 import { Link } from "react-router-dom";
+import "../project.css";
+
+const FLEX_DIRECTION = ["column-reverse", "row", "column", "row-reverse"];
 
 export default class ArticleButton extends React.Component {
   render() {
     const { item, isHover, index } = this.props;
     return (
-      <Link to={item.link} className="link" onMouseEnter={this.props.onHover}>
-        {isHover && (
-          <img
-            style={{ transform: `rotate(${index * 90}deg)`, width: "5vw" }}
-            alt="error"
-            src={require(`../../../assets/icon/${item.img}`)}
-          />
-        )}
-        <p
-          style={{ transform: `rotate(${index * 90}deg)` }}
-          className="btnTitle"
+      <Link
+        to={`/${item.link}`}
+        className="link"
+        onMouseEnter={this.props.onHover}
+        onMouseOut={this.props.onOut}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: FLEX_DIRECTION[index],
+            alignItems: index === 2 || index === 3 ? "flex-end" : "flex-start",
+            width: "25vw",
+            height: "50vh",
+          }}
         >
-          {item.title}
-        </p>
+          <img
+            src={require(`../../../assets/icon/${item.title}`)}
+            alt={item.title}
+            style={{
+              width: index === 1 || index === 3 ? "13vh" : "20vw",
+              height: index === 1 || index === 3 ? "20vw" : "13vh",
+            }}
+          />
+          {isHover && (
+            <img
+              style={{
+                width: "5vw",
+              }}
+              alt="error"
+              src={require(`../../../assets/icon/${item.img}`)}
+            />
+          )}
+        </div>
       </Link>
     );
   }
