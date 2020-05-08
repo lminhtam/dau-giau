4e446dce9ecb56d1cce381ac9904dbe66b095386
Luong Minh Tam <tam.luong-minh@internal.banvien.com>
2020-05-08 18:23:52 +0700

    fix filter prj

--- src/pages/Project/component/articleButton.jsx
+++ src/pages/Project/component/articleButton.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../project.css";
 import { animated, useSpring } from "react-spring";
+import { useState } from "react";
 
 const FLEX_DIRECTION = ["column-reverse", "row", "column", "row-reverse"];
 const CONTENT_FLEX = ["row", "column", "row-reverse", "column-reverse"];
 
 export default function ArticleButton(props) {
   const { item, isHover, index } = props;
+  const [buttonClick, setButtonClick] = useState(false);
+  const iconAnimate = useSpring({
+    opacity: isHover ? 1 : 0,
+  });
   const filter = {
     article0: useSpring({
       left: isHover ? "53vw" : "50vw",
@@ -26,6 +31,20 @@ export default function ArticleButton(props) {
       bottom: isHover ? "55vh" : "50vh",
     }),
   };
+  const title = {
+    animate0: useSpring({
+      marginBottom: isHover ? "0vh" : "-5vh",
+    }),
+    animate1: useSpring({
+      marginLeft: isHover ? "2vw" : "-5vw",
+    }),
+    animate2: useSpring({
+      marginTop: isHover ? "0vh" : "-5vh",
+    }),
+    animate3: useSpring({
+      marginRight: isHover ? "2vw" : "-5vw",
+    }),
+  };
   return (
     <div
       style={{
@@ -36,20 +55,22 @@ export default function ArticleButton(props) {
         height: "50vh",
       }}
     >
-      <animated.div style={filter[`article${index}`]}>
-        <Link
-          to={item.link}
-          className="link"
-          onMouseEnter={() => props.onHover()}
-          onMouseOut={() => props.onOut()}
+      <Link
+        className="link"
+        onMouseEnter={buttonClick ? () => {} : () => props.onHover()}
+        onMouseOut={buttonClick ? () => {} : () => props.onOut()}
+        onClick={() => setButtonClick(!buttonClick)}
+      >
+        <animated.div
+          style={buttonClick ? {} : filter[`article${index}`]}
           id={`filterPrj${index}`}
         />
-      </animated.div>
+      </Link>
       <Link
-        to={item.link}
         className="link"
-        onMouseEnter={() => props.onHover()}
-        onMouseOut={() => props.onOut()}
+        onMouseEnter={buttonClick ? () => {} : () => props.onHover()}
+        onMouseOut={buttonClick ? () => {} : () => props.onOut()}
+        onClick={() => setButtonClick(!buttonClick)}
         style={{
           width: index === 1 || index === 3 ? "13vh" : "20vw",
           height: index === 1 || index === 3 ? "20vw" : "13vh",
@@ -64,7 +85,7 @@ export default function ArticleButton(props) {
           }}
         />
       </Link>
-      {isHover && (
+      {(isHover || buttonClick) && (
         <div
           style={{
             display: "flex",
@@ -72,21 +93,28 @@ export default function ArticleButton(props) {
             alignItems: index === 3 || index === 0 ? "flex-end" : "flex-start",
           }}
         >
-          <img
-            style={{
-              width: "5vw",
-            }}
+          <animated.img
+            style={buttonClick ? {} : iconAnimate}
+            id="iconStyle"
             alt="error"
             src={require(`../../../assets/icon/${item.img}`)}
           />
           {item.items && item.items.length > 0 ? (
-            <div className={`title${index}`}>
+            <animated.div
+              className={`title${index}`}
+              style={buttonClick ? {} : title[`animate${index}`]}
+            >
               {item.items.map((data) => (
                 <span>{data.title}</span>
               ))}
-            </div>
+            </animated.div>
           ) : (
-            <span className={`title${index}`}>COMING SOON</span>
+            <animated.span
+              style={buttonClick ? {} : title[`animate${index}`]}
+              className={`title${index}`}
+            >
+              COMING SOON
+            </animated.span>
           )}
         </div>
       )}
